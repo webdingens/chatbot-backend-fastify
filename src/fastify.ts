7fe593ac3e56ee1ed74fe2f407c2127adaa1ff18
Martin Senk <mail@webdingens.de>
2025-01-28 20:45:37 +0100

    fixed cors error for local testing

--- src/fastify.ts
+++ src/fastify.ts
@@ -3,4 +3,24 @@ const fastify = Fastify({
   logger: true,
 });
 
+fastify.addHook("onRequest", (request, reply, done) => {
+  reply.header("Access-Control-Allow-Origin", "*");
+  reply.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS"
+  );
+  reply.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Accept"
+  );
+
+  // Handle preflight requests
+  if (request.method === "OPTIONS") {
+    reply.status(200).send();
+    return;
+  }
+
+  done();
+});
+
 export default fastify;
